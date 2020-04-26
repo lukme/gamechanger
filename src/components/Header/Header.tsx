588fcc43fb1ef840f09f93214ad15f4328498290
lukme <lukme00@gmail.com>
2020-04-26 22:02:10 +0200

    added transition to header based on scroll (v1)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.style.scss';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
-    const [isNavboxOpened, setIsNavboxOpened] = useState<boolean>();
+    const [whichNavboxOpened, setwhichNavboxOpened] = useState<'ARTYKUŁY' | 'RECENZJE'>(),
+        [showWholeHeader, setShowWholeHeader] = useState<boolean>(true),
+        headerClassName = showWholeHeader ? 'header__container' : 'header__container header--small';
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, {
+            capture: true,
+            passive: true
+        })
+    });
+
+    const handleScroll = () => {
+        setShowWholeHeader(
+            window.scrollY <= 60 ? true : false
+        );
+    }
 
     return (
-        <div className="header">
-            <div className='header__container'>
+        <div className='header'>
+            <div className={headerClassName}>
                 <Link to='/' className='header__logo'>
                     GAMECHANGER
                 </Link>
                 <ul className='header__nav'>
                     <li
-                        onClick={() => setIsNavboxOpened(!isNavboxOpened)}
+                        onClick={() => setwhichNavboxOpened('ARTYKUŁY')}
                         className='header__item'
                     >
                         <p className='header__link'>
@@ -21,7 +36,7 @@ export const Header = () => {
                         </p>
                     </li>
                     <li
-                        onClick={() => setIsNavboxOpened(!isNavboxOpened)}
+                        onClick={() => setwhichNavboxOpened('RECENZJE')}
                         className='header__item'
                     >
                         <p className='header__link'>
@@ -30,13 +45,13 @@ export const Header = () => {
                     </li>
                 </ul>
             </div>
-            {isNavboxOpened && (
+            {whichNavboxOpened && (
                 <div className="navbox">
                     <h2 className='navbox__header'>
-                        RECENZJE
+                        {whichNavboxOpened}
                     </h2>
                     <div className="navbox__content">
-                        
+
                     </div>
                 </div>
             )}
